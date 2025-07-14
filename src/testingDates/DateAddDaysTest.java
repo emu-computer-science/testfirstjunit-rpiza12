327f669a7e6785c5f7aad086ca3e0eec6f8eefc8
rpiza12 <rpiza@emich.edu>
2025-07-14 13:42:58 -0400

    Tests written

--- src/testingDates/DateAddDaysTest.java
+++ src/testingDates/DateAddDaysTest.java
@@ -16,13 +16,20 @@ public class DateAddDaysTest {
 	public void testAddOneDayCrossMonth() {
 		Date dateA = new Date(7, 31, 2025);
 		Date dateB = new Date(8, 1, 2025);
+		Date dateC = new Date(2, 28, 2024);
+		Date dateD = new Date(2, 29, 2024);
+		Date dateE = new Date(3, 1, 2024);
 		
 		dateA.addOneDay();
 		assertEquals(dateB, dateA);
+		dateC.addOneDay();
+		assertEquals(dateD, dateC);
+		dateC.addOneDay();
+		assertEquals(dateE, dateC);
 	}
 	
 	@Test
-	public void testAddOneDayCrossYea() {
+	public void testAddOneDayCrossYear() {
 		Date dateA = new Date(12, 31, 2025);
 		Date dateB = new Date(1, 1, 2026);
 		
--- src/testingDates/DateSetDateTest.java
+++ src/testingDates/DateSetDateTest.java
@@ -5,5 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DateSetDateTest {
-
+	@Test
+	public void testSetDate() { // (String, int, int)
+		Date dateA = new Date(7, 21, 2025);
+		dateA.setDate("July", 14, 2025);
+		
+		Date dateB = new Date(7, 21, 2025);
+		dateB.setDate("bubbles", 14, 2025);
+		
+		Date dateC = new Date(7, 27, 2025);
+		dateC.setDate("july", 14, 2025);
+		
+		assertEquals(new Date("July", 14, 2025), dateA);
+		assertEquals(new Date("July", 21, 2025), dateB);
+		assertEquals(new Date("July", 27, 2025), dateC);
+	}
 }
